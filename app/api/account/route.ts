2411e13ebe254c1c8c5cca1d77845accbfe5f2e0
Nurjon <nurjon2477@gmail.com>
2023-12-23 14:43:25 +0500

    post function connect to database

---
+++ app/api/account/route.ts
@@ -0,0 +1,50 @@
+import {connectToDatabase} from "@/lib/mongoose";
+import {NextResponse} from "next/server";
+import Account from "@/database/account";
+import {hash} from "bcryptjs"
+
+export const dynamic = "force-dynamic";
+
+//
+export async function POST(req: Request) {
+    try {
+        await connectToDatabase();
+        const {name, pin, uid} = await req.json();
+
+        const isExist = await Account.findOne({name});
+        const allAccounts = await Account.find({uid});
+
+        if(isExist) {
+            return NextResponse.json({success: false, message: "You already have an account"})
+        }
+
+        if(allAccounts && allAccounts.length === 4) {
+            return NextResponse.json({success: false, message: "You can only have 4 accounts"})
+        }
+
+        const hashPin = await hash(pin, 10);
+
+        const account = await Account.create({name, pin: hashPin, uid});
+
+        return NextResponse.json({account})
+    }catch (e) {
+        return NextResponse.json({success: false, message: "Something went wrong"})
+    }
+}
+
+//Get all accounts
+
+export async function GET(req:Request) {
+    try {
+      await connectToDatabase()
+
+        const {searchParams} = new URL(req.url)
+        const  uid = searchParams.get("uid")
+
+        const account = await Account.find(){uid}
+
+        return NextResponse.json({account})
+    }catch (e) {
+        return NextResponse.json({success : false message : "Something went wrong"})
+    }
+}
---
+++ database/account.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import {options} from "preact";
+
+const accountSchema = new mongoose.Schema({
+    uid:String,
+    name:String,
+    pin:String
+}, {timestamps:true})
+
+const Account = mongoose.models.Account || mongoose.model("Account" , accountSchema)
+
+export default  Account
