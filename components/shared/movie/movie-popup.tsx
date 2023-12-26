8408000bf8db36ac5fe63669e7abc4728e959eb7
Nurjon <nurjon2477@gmail.com>
2023-12-26 12:49:01 +0500

    teraoler movies

--- components/shared/movie/movie-popup.tsx
+++ components/shared/movie/movie-popup.tsx
@@ -1,22 +1,45 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useGlobalContext } from "@/context";
 import { useEffect, useState } from "react";
-import { getMovieDetails } from "@/lib/api";
-import { MovieDetailsProps } from "@/types";
+import { getMovieDetails, getSimilarMovies } from "@/lib/api";
+import { MovieDetailsProps, MovieProps } from "@/types";
 import ReactStars from 'react-stars'
+import MovieItem from "./movie-item";
+import ReactPlayer from "react-player";
 
 const MoviePopup = () => {
       const [movieDetails, setMovieDetails] = useState<MovieDetailsProps | null>(null);
+      const [similarMovies, setSimilarMovies] = useState<MovieProps[]>([])
+      const [key, setKey] = useState<string>()
 
       const { open, setOpen, movie } = useGlobalContext()
+
 
       useEffect(() => {
             const getMovie = async () => {
                   try {
                         const extractMovieDetails = await getMovieDetails(movie?.type, movie?.id)
-                        setMovieDetails(extractMovieDetails)
+                        const similarMovies = await getSimilarMovies(movie?.type, movie?.id)
+
+                        const results = similarMovies.map((movie: MovieProps) => ({ ...movie, type: extractMovieDetails?.type, addedToFavorites: false }))
+
+                        setMovieDetails(extractMovieDetails?.data)
+                        setSimilarMovies(results)
+
+                        const findIndexOfTrailer = extractMovieDetails?.data?.videos?.results?.length ?
+                              extractMovieDetails?.data?.videos?.results?.findIndex((item: { type: string }) => item.type === "Trailer") : -1
+
+                        const findIndexOfClip = extractMovieDetails?.data?.videos?.results?.length ?
+                              extractMovieDetails?.data?.videos?.results?.findIndex((item: { type: string }) => item.type === "Clip") : -1
+
+                        const key = findIndexOfTrailer !== -1 ? extractMovieDetails?.data?.videos?.results[findIndexOfTrailer]?.key :
+                              findIndexOfClip !== -1 ? extractMovieDetails?.data?.videos?.results[findIndexOfClip]?.key : null
+
+                        setKey(key)
+
                   } catch (e) {
                         console.log(e)
                   } finally {
@@ -24,13 +47,28 @@ const MoviePopup = () => {
                   }
             }
 
-            getMovie()
-      }, [movie, open]);
+            if (movie !== null) {
+                  getMovie()
+            }
+      }, [movie]);
+   
 
       return (
             <Dialog open={open} onOpenChange={setOpen}>
-                  <DialogContent className={"max-w-4xl"}>
-                        <div className={"w-full h-56 bg-black"}></div>
+                  <DialogContent className={"max-w-6xl !scrollbar-thumb-red-600 !scrollbar-track-transparent  max-h-[90vh] overflow-y-auto !scrollbar-thin"}>
+                        <div className={" relative  pt-[56.25%]"}>
+
+                              <ReactPlayer
+                               
+                               url={`https://www.youtube.com/watch?v=${key}`}
+                               width={'100%'}
+                               height={'100%'}
+                               style={{position: 'absolute' , top : '0' , left : '0'}}
+                               playing
+                               controls
+
+                              />
+                        </div>
 
                         <div className={"flex flex-col space-y-4"}>
                               <h1 className={"text-2xl md:text-4xl lg:text-4xl font-bold line-clamp-1"}>
@@ -45,6 +83,22 @@ const MoviePopup = () => {
                                     <span className={"text-green-400 font-semibold"}>{movieDetails?.release_date ? movieDetails?.release_date.split("-")[0] : "2023"}</span>
                                     <div className="inline-flex border-2 border-white/40 text-green-400 font-semibold rounded px-2">HD</div>
                               </div>
+                        </div>
+
+                        <div className=" bg-black p-4 rounded-md shadow-2xl">
+                              <h2 className=" mt-2 mb-6 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition-colors duration-200  hover:text-white md:text-2xl">
+                                   More Like This
+                              </h2>
+
+                              <div className=" grid grid-cols-4 gap-3 items-center scrollbar-hide md:p-2">
+                                    { similarMovies && similarMovies.length && similarMovies
+                                    .filter(item => item.backdrop_path !== null && item.poster_path !== null)
+                                    .map(movie => (
+                                          <MovieItem movie={movie} key={movie.id} />
+                                    )) }
+
+                              </div>
+
                         </div>
                   </DialogContent>
             </Dialog>
